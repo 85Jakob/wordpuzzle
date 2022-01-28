553b7f2a803ee8780cc229fa51910a25e3988dc1
85Jakob <jacobadoney@gmail.com>
2022-01-28 07:02:22 -0600

    initial commit

--- hiddenword.cpp
+++ hiddenword.cpp
@@ -1,17 +1,39 @@
-#include<iostream>
-#include<ctime>
- 
-int main(void)
+//By Jacob Doney
+//Generates a random word
+
+#include "hiddenword.h"
+
+int randomNum(void)
 {
-    // This program will create different sequence of
-    // random numbers on every program run
- 
-    // Use current time as seed for random generator
     
-    int max = 100;
+    int max = 11;
+    int num;
     srand(time(0));
  
-    std::cout << rand() %max << std::endl;
+    num = rand() %max;
  
-    return 0;
+    return num;
+}
+
+
+void hiddenWord(char *hword)
+{
+    int num = randomNum();
+    char word[10][5] =
+    {
+	{'h','e','l','l','o'}, //1
+	{'t','a','c','o','s'}, //2
+	{'r','o','y','a','l'}, //3
+	{'p','l','a','n','t'}, //4
+	{'t','o','w','e','r'}, //5
+	{'l','i','g','h','t'}, //6
+	{'p','h','o','n','e'}, //7
+	{'f','l','o','u','r'}, //8
+	{'f','i','r','s','t'}, //9
+	{'s','t','o','r','e'} //10
+    };
+	
+    for(int i = 0; i < 5; i++){
+        hword[i] = word[num][i];
+    }
 }
