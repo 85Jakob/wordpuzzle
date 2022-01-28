dd2e4748985779aeb7fd8ff795e26cd1342d6085
85Jakob <jacobadoney@gmail.com>
2022-01-28 07:01:35 -0600

    initial commit

---
+++ hiddenword.h
@@ -0,0 +1,13 @@
+//By Jacob
+//directives for hiddenword.cpp
+
+#ifndef HIDDENWORD_H
+#define HIDDENWORD_H
+
+#include<ctime>
+#include<iostream>
+
+int randomNum(void);
+void hiddenWord(char*);
+
+#endif //HIDDENWORD_H
