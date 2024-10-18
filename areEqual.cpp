363eb7dc2747929632a884bff5787f037b71024d
Jacob <JacobADoney@gmail.com>
2024-10-18 12:42:56 -0500

    Update areEqual.cpp

--- areEqual.cpp
+++ areEqual.cpp
@@ -4,7 +4,7 @@
 
 bool areEqual(char *arr1, char *arr2, int size){
 	bool isEqual = true;
-    for(int i=0; i < size; i++){
+	for(int i=0; i < size; i++){
 		if(arr1[i] != arr2[i])
 			isEqual = false;
 	}
